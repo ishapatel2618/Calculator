bccaa947eca60e4e0d94242fdbb4380952db51d5
Isha Patel <ishapatel2618@gmail.com>
2020-06-17 17:41:32 -0400

    Final Commit - Review done

---
+++ Product.Tests/CalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace Product.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString()
+        {
+            int result = Calculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        public void Add_OneNumberString(string numbers, int expected)
+        {
+            int result = Calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+        
+        [TestCase("1,2",3)]
+        [TestCase("2,3", 5)]
+        public void Add_TwoNumbersString(string numbers, int expected)
+        {
+            int result = Calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Add_MultipleNumbers()
+        {
+            int result = Calculator.Add("1,2,3");
+
+            Assert.AreEqual(6,result);
+        }
+        [Test]
+        public void Add_NoNumberStringWithelimeterLine()
+        {
+            int result = Calculator.Add("//,\n");
+
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void Add_OneNumberStringWithDelimeterLine()
+        {
+            int result = Calculator.Add("//,\n1");
+
+            Assert.AreEqual(1, result);
+        }
+        [Test]
+        public void Add_TwoNumberStringWithAlternativeDelimeter()
+        {
+            int result = Calculator.Add("//;\n1;2");
+
+            Assert.AreEqual(3,result);
+        }
+    }
+}
---
+++ Product/Calculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Product
+{
+    public class Calculator
+    {
+        private const string DELIMETER_LINE_INDICATOR = "//";
+        private static string _delimeter = ",";
+
+        public static int Add(string numbers)
+        {
+            if (HasDelimeterLine(numbers))
+            {
+                ParseDelimeter(numbers);
+                numbers = GetNumbers(numbers);
+            }
+
+            if (IsEmptyString(numbers))
+                return HandleEmptyString();
+
+            if (HasMultipleNumbers(numbers))
+            {
+                return HandleMultipleNumbers(numbers);
+            }
+
+
+            return HandleOneNumber(numbers);
+
+        }
+
+        private static void ParseDelimeter(string numbers)
+        {
+            _delimeter = numbers.Substring(2, 1);
+        }
+
+        private static string GetNumbers(string numbers)
+        {
+            string[] numParts = numbers.Split(char.Parse("\n"));
+            numbers = numParts[1];
+            return numbers;
+        }
+
+        private static bool HasDelimeterLine(string numbers)
+        {
+            return numbers.StartsWith(DELIMETER_LINE_INDICATOR);
+        }
+
+        private static int HandleMultipleNumbers(string numbers)
+        {
+            string[] nums = numbers.Split(_delimeter.ToCharArray());
+            int total = 0;
+
+            foreach (string num in nums)
+            {
+                
+                total += HandleOneNumber(num);
+            }
+            return total;
+           
+        }
+
+        private static bool HasMultipleNumbers(string numbers)
+        {
+            return numbers.Contains(_delimeter);
+        }
+
+        private static int HandleOneNumber(string numbers)
+        {
+            return int.Parse(numbers);
+        }
+
+        private static int HandleEmptyString()
+        {
+            return 0;
+        }
+
+        private static bool IsEmptyString(string numbers)
+        {
+            return numbers.Length == 0;
+        }
+    }
+}
